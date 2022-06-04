f36ebd64dbf6ea5b488ad4b28a4d1e3ef931f092
Egor.Andreev <egor_andreev@unilever.com>
2022-06-04 13:48:16 +0300

    first version of project

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-##second project master    file
+def decorator_func(function_name):
+    def inner_decorator(num1, num2):
+        print("decoration!!! in progress")
+        c = function_name(num1, num2)
+        return c
+    return inner_decorator
+
+@decorator_func
+def calculator_sum(a, b):
+    c = a+b
+    print(c)
+    return c
+
+p = calculator_sum(5, 3)
+print(p)

369f0965ca4f707ae345b758d63aab35363e5612
Egor.Andreev <egor_andreev@unilever.com>
2022-06-04 14:06:01 +0300

    decorator funcion test with argumen and return

--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
 def decorator_func(function_name):
-    def inner_decorator(num1, num2):
+    def inner_decorator(num1, num2): #1
         print("decoration!!! in progress")
-        c = function_name(num1, num2)
-        return c
+        c = function_name(num1, num2) #2
+        return c #5
     return inner_decorator
 
 @decorator_func
-def calculator_sum(a, b):
+def calculator_sum(a, b):#3
     c = a+b
     print(c)
-    return c
+    return c #4
 
 p = calculator_sum(5, 3)
+#6
 print(p)
+#returns result of calculation. Последовательность передачи аргументов в комментариях
